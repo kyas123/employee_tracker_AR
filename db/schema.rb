dc391dd747d1fab9bf4011ceae4d8430fc54ee2a
patrickdavid &  Kyle Smith <student@epicodus.com>
2014-08-25 15:14:21 -0700

    update the schema for the divisio_employees table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825191553) do
+ActiveRecord::Schema.define(version: 20140825213023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,11 @@ ActiveRecord::Schema.define(version: 20140825191553) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "divisions_employees", force: true do |t|
+    t.integer "employee_id"
+    t.integer "division_id"
   end
 
   create_table "employees", force: true do |t|
