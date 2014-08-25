44f7186c0c93d96e5f1886d567546559b4b56008
patrickdavid &  Kyle Smith <student@epicodus.com>
2014-08-25 14:10:18 -0700

    add the boolean for the added column

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140825171423) do
+ActiveRecord::Schema.define(version: 20140825191553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20140825171423) do
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.boolean  "remote"
   end
 
 end
