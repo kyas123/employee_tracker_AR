d0956168a5df8bc18cdef8822efee800a662e696
patrickdavid &  Kyle Smith <student@epicodus.com>
2014-08-25 15:34:05 -0700

    write the spec for the relationship between employee and division

--- spec/employee_spec.rb
+++ spec/employee_spec.rb
@@ -1,11 +1,10 @@
 require 'spec_helper'
 
 describe Employee do
-#   it 'can be added to a division' do
 
-# end
+  it { should have_and_belong_to_many :divisions}
 
-  it 'will return all employees that work remotely' do
+  it 'will return all employees that work locally' do
     current_employees = (1..2).to_a.map { |number| Employee.create(name: "employee #{number}", remote: true)}
     remote_employee = Employee.create(name: 'remote_employee', remote: false)
     expect(Employee.local_employees).to eq current_employees
